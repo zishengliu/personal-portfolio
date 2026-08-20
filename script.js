fb0e0ae8fb8593f9094b7dead7f9355d09ada15f
zishengliu <129256370+zishengliu@users.noreply.github.com>
2026-08-20 15:05:45 +0800

    Add website interactions

---
+++ script.js
@@ -0,0 +1,2 @@
+const observer = new IntersectionObserver((entries)=>entries.forEach(e=>{if(e.isIntersecting)e.target.classList.add('show')}),{threshold:.12});
+document.querySelectorAll('.reveal').forEach(el=>observer.observe(el));
